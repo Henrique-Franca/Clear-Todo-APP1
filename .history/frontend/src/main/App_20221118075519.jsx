2533fa012ed937fd34ee34258da52e01bd2620e3
Henrique França <94202846+Henrique-Franca@users.noreply.github.com>
2022-11-18 07:59:24 -0300

    Componentes Todo e About

--- .history/frontend/src/main/App_20221118075519.jsx
+++ .history/frontend/src/main/App_20221118075519.jsx
@@ -3,12 +3,10 @@ import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
 import Todo from '../todo/todo'
-import About from '../about/about'
 
 export default props =>(
     <div className="container">
-        <Todo/>
-        <About/>
+    <h1>Teste</h1>
   </div>
 )
 
