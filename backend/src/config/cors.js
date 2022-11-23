d0e8c3ecf56e599f191109d8293d40ce4c9a7e89
Henrique França <94202846+Henrique-Franca@users.noreply.github.com>
2022-11-23 07:36:28 -0300

    backend resolvido

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,14 +1,6 @@
-const allowCors = require('./cors')
-const bodyParser = require('body-parser')
-const express = require('express')
-const cors = require('cors')
-const server = express()
-const port = 3003
-server.use(bodyParser.urlencoded({ extended: true}))
-server.use(bodyParser.json())
-server.use(allowCors)
-server.use(cors())
-server.listen(port, function() {
-    console.log(`BACKEND is running on port ${port}.`)
-})
-module.exports = server
+module.exports = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+}
--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,13 +1,16 @@
-module.exports = (req, res, next) => {
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Methods",
-      "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-    );
-    res.header(
-      "Access-Control-Allow-Headers",
-      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-    );
-    next();
-  };
+const port = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+const allowCors = require('./cors')
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+server.use(allowCors)
+
+server.listen(port, function() {
+    console.log(`BACKEND is running on port ${port}.`)
+})
+
+module.exports = server
